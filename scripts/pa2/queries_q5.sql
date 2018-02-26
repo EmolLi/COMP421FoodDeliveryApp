c1932086956b442731ccc0e1c116c0aedcc0c2ab
Yuan Zhang <yuan.zhang5@mail.mcgill.ca>
2018-02-26 11:24:16 -0500

    update queries

--- scripts/pa2/queries_q5.sql
+++ scripts/pa2/queries_q5.sql
@@ -9,23 +9,26 @@
 
 -- queries
 SELECT * FROM creditCards
-WHERE holder_name = 'RACHEL DERRICK';
+WHERE holder_name LIKE 'M%'
+LIMIT 5;
 
 SELECT * FROM creditCards
 WHERE expiry_date > '2018-03-01' ORDER BY holder_name ASC
 LIMIT 5;
 
--- aggregations
-SELECT avg(overall_rating) FROM restaurants
-WHERE opening_hours = '10:00';
+-- aggregations, group by, subqueries
+SELECT license_id, avg(rating) FROM reviews
+WHERE EXISTS (SELECT restaurants.license_id FROM restaurants
+				WHERE restaurants.opening_hours = '10:00')
+GROUP BY license_id
+LIMIT 5;
 
 -- group by, aggregations
 SELECT borough, count(*) AS TotalDeliveryStaffs FROM deliveryStaffs
-WHERE borough IN ('Outremont', 'Saint-Laurent')
-GROUP BY borough
-LIMIT 5;
+WHERE borough IN ('Outremont', 'Saint-Laurent', 'Ville-Marie', 'Le Plateau-Mont-Royal', 'Lachine')
+GROUP BY borough;
 
--- subqueries
+-- subqueries // TODO add data
 SELECT c.name FROM customers c
 WHERE EXISTS (SELECT d.cell_phone_number FROM deliveryStaffs d
               WHERE d.cell_phone_number = c.cell_phone_number);
