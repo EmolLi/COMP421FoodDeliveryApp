2dcc62be905ed0448be12a6cbb3fcdbfdcea7d3d
Yuan Zhang <yuan.zhang5@mail.mcgill.ca>
2018-02-26 21:31:18 -0500

    add script execution logs

--- scripts/pa2/queries_q5.sql
+++ scripts/pa2/queries_q5.sql
@@ -10,25 +10,33 @@
 -- queries
 SELECT * FROM creditCards
 WHERE holder_name LIKE 'M%'
-LIMIT 5;
+LIMIT 7;
 
 SELECT * FROM creditCards
-WHERE expiry_date > '2018-03-01' ORDER BY holder_name ASC
+WHERE expiry_date < '2018-03-31' ORDER BY holder_name ASC
 LIMIT 5;
 
--- aggregations, group by, subqueries
+SELECT r.name FROM restaurants r
+WHERE EXISTS (SELECT addresses.aid = r.aid FROM addresses
+			  	WHERE addresses.borough = 'Saint-Laurent')
+LIMIT 7;
+
+-- aggregations, exists, group by, subqueries
 SELECT license_id, avg(rating) FROM reviews
 WHERE EXISTS (SELECT restaurants.license_id FROM restaurants
-				WHERE restaurants.opening_hours = '10:00')
+			  WHERE restaurants.opening_hours = '10:00')
 GROUP BY license_id
-LIMIT 5;
+LIMIT 7;
 
--- group by, aggregations
+-- aggregations, exists
 SELECT borough, count(*) AS TotalDeliveryStaffs FROM deliveryStaffs
 WHERE borough IN ('Outremont', 'Saint-Laurent', 'Ville-Marie', 'Le Plateau-Mont-Royal', 'Lachine')
 GROUP BY borough;
 
+
+/*
 -- subqueries // TODO add data
 SELECT c.name FROM customers c
 WHERE EXISTS (SELECT d.cell_phone_number FROM deliveryStaffs d
               WHERE d.cell_phone_number = c.cell_phone_number);
+*/
