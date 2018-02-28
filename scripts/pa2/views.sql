9139b53e89a6f736c7b82f09024dc5afab9d0c1e
faeii <35305858+faeii@users.noreply.github.com>
2018-02-27 23:21:48 -0500

    Update views.sql

--- scripts/pa2/views.sql
+++ scripts/pa2/views.sql
@@ -4,26 +4,19 @@
 -- notes: assumes opening hours are before closing hours on the same day
 -- e.g. 11am - 1am the next day is not allowed
 
--- command
 CREATE VIEW availableDishes AS
 SELECT r.name as restaurant, d.name, description, price, type
 FROM restaurants r, dishes d
 WHERE r.license_id=d.license_id
-AND CURRENT_TIME BETWEEN opening_hours AND Closing_hours
-ORDER BY r.name
+AND CURRENT_TIME BETWEEN opening_hour AND closing_hour
+ORDER BY r.name;
 
--- 2. customers sorted by their total spending on all orders
--- notes: does the table 'orders' have a total_cost attribute?
 
--- command if order has total_cost attribute
-CREATE VIEW spendingPerPerson AS
-SELECT cell_phone_number, sum(total_cost) FROM orders
-GROUP BY cell_phone_number
-ORDER BY sum(total_cost) DESC;
+-- 2. customers who have made orders sorted by their total spending on all orders
+-- notes: customers who have not made any orders will not appear in this view
 
--- command if order does not have total_cost attribute
 CREATE VIEW spendingPerPerson AS
 SELECT cell_phone_number, sum(quantity*price) FROM orders o, contains c, dishes d
 WHERE o.oid=c.oid AND c.license_id=d.license_id AND c.name=d.name
 GROUP BY cell_phone_number
-ORDER BY sum(total_cost) DESC;
+ORDER BY sum(quantity*price) DESC;
