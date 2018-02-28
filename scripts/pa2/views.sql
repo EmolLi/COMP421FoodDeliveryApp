c4751b93b4d49249f8f28436ccbdd021d1b5b31f
faeii <35305858+faeii@users.noreply.github.com>
2018-02-27 23:57:06 -0500

    Update views.sql

--- scripts/pa2/views.sql
+++ scripts/pa2/views.sql
@@ -1,6 +1,7 @@
 -- q7 - two create view commands
 
--- 1. shows all dishes from restaurants currently open, sorted by restaurant
+-- 1. 
+-- Description: shows all dishes from restaurants currently open, sorted by restaurant
 -- notes: assumes opening hours are before closing hours on the same day
 -- e.g. 11am - 1am the next day is not allowed
 
@@ -11,8 +12,11 @@ WHERE r.license_id=d.license_id
 AND CURRENT_TIME BETWEEN opening_hour AND closing_hour
 ORDER BY r.name;
 
+-- is this updatable? why/why not?
 
--- 2. customers who have made orders sorted by their total spending on all orders
+
+-- 2. 
+-- Description: lists customers who have made orders sorted by their total spending on all orders
 -- notes: customers who have not made any orders will not appear in this view
 
 CREATE VIEW spendingPerPerson AS
@@ -20,3 +24,5 @@ SELECT cell_phone_number, sum(quantity*price) FROM orders o, contains c, dishes 
 WHERE o.oid=c.oid AND c.license_id=d.license_id AND c.name=d.name
 GROUP BY cell_phone_number
 ORDER BY sum(quantity*price) DESC;
+
+-- this view is not updatable because it uses aggregate functions such as sum and GROUP BY
