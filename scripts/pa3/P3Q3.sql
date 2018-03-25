88fcbe73cdfdcd8b9f40110e82e61261add797b0
yz9 <Yuan.zhang5@mail.mcgill.ca>
2018-03-25 19:19:00 -0400

    Update P3Q3.sql

--- scripts/pa3/P3Q3.sql
+++ scripts/pa3/P3Q3.sql
@@ -1,7 +1,7 @@
 -- a
 CREATE INDEX restaurantName ON restaurants(name);
-DROP INDEX restaurantName;
+--DROP INDEX restaurantName;
 
 -- b
 CREATE INDEX orderStatus ON orders(status, cell_phone_number);
-DROP INDEX orderStatus;
+--DROP INDEX orderStatus;
